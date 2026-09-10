20073ef96eba76d693d7d9a1a57851086d6469ad
brandonj2004 <brandonjustin2004@gmail.com>
2026-09-10 13:05:30 -0400

    Fixed indentation

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello world");
-    }
+  public static void main(String[] args) {
+    System.out.print("Hello world");
+  }
 }
